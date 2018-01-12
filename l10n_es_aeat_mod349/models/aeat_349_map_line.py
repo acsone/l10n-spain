eb003e3e4113a14ed5ecb7f2ee8b8b6a3ab668fb
Jordi Ballester <jordi.ballester@eficent.com>
2018-01-12 17:50:09 +0100

    refactoring de l10n_es_aeat_mod349. Mejoras incluídas: * Mejora del rendimiento. * Apuntes contables como fuente de datos. El motor del informe se construye a partir de apuntes   contables, y la información de facturas se consulta cuando se necesita. De este modo aseguramos que   los resultados serán consistentes con los otros modelos de AEAT, que también toman como fuente de   datos los apuntes contables. Así evitamos posibles inconsistencias. * Nuevo informe Qweb, que sustituye al antiguo RML * Correcciones para exporación a formato BOE. Permite importar el modelo a la web de hacienda. * Permite borrar operaciones que no se quiere declarar. El usuario puede eliminar las operaciones que   no desee incluir en la presentación. Esto será especialmente útil en el caso de presentaciones   complementarias. * En declaraciones normales y complementarias se excluyen automáticamente operaciones ya   presentadas anteriormente. * Script de migración para tener en cuenta los cambios en el modelo de datos.

--- l10n_es_aeat_mod349/models/aeat_349_map_line.py
+++ l10n_es_aeat_mod349/models/aeat_349_map_line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Luis M. Ontalba <luis.martinez@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from openerp import api, models, fields
 
